6128cd36577e68ea3043d2d4ab59d075e159a723
yuvaroshit <38933993+yuvaroshit@users.noreply.github.com>
2018-05-07 11:32:36 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 // Models
-var Product = require('../models/product');
+var User = require('../models/user');
 
 // Routes
-Product.methods(['get', 'put', 'post', 'delete']);
-Product.register(router, '/products');
+User.methods(['get', 'put', 'post', 'delete']);
+User.register(router, '/user');
 
 // Return router
 module.exports = router;
